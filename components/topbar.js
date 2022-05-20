a673ff0156c06454095e779255daa54cb9dcf819
drafffffff <i0.0i@qq.com>
2022-05-20 18:41:24 +0800

    sdf

---
+++ components/topbar.js
@@ -0,0 +1,19 @@
+import styles from "./topbar.module.scss"
+import Image from "next/image"
+import {useRouter} from 'next/router'
+
+export default function TopBar({title}) {
+    const router = useRouter()
+    return (
+        <div className={styles.topbar}>
+            <div className={styles.left} onClick={() => {
+                router.back()
+            }}>
+                <Image src={require("../public/back.png")} alt={"back"} width={92} height={92}/>
+            </div>
+            <div className={styles.mid}>
+                {title}
+            </div>
+        </div>
+    )
+}
